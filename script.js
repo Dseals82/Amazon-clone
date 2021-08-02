f5ac6c0802b20e2aadd75c510fbca6a5b08b12eb
Tech Manager <techmanager@techs-mbp.myfiosgateway.com>
2021-08-02 09:17:57 -0400

    database update

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-     function getItems(){
-        db.collection("items").get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                console.log(`${doc.id} => ${doc.data()}`);
+function getItems(){
+    db.collection("items").get().then((querySnapshot) => {
+        let items = [];
+        querySnapshot.forEach((doc) => {
+            // console.log(`${doc.id} => ${doc.data()}`);
+            items.push({
+                id: doc.id,
+                image: doc.data().img,
+                name: doc.data().name,
+                make: doc.data().make,
+                rating: doc.data().rating,
+                price: doc.data().price
             });
-        });  
-    }
+        });
+        generateItems(items);
+    });
+};
 
-    getItems();
+function generateItems(items){
+    items.forEach((item)=>{
+
+    })
+};
+
+getItems();
