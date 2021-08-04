af09b859b5587332a8d249d77d82a3139e6f4917
Tech Manager <techmanager@techs-mbp.myfiosgateway.com>
2021-08-04 15:06:25 -0400

    added js library called numeral.js to convert numbers into proper currency format

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function generateItems(items){
             ⭐⭐⭐⭐⭐ ${item.rating}
         </div>
         <div class="product-price font-bold text-gray-700 text-lg">
-            $${item.price}
+            ${numeral(item.price).format('$0,0.00')}
         </div>
         `
        let addToCartEl = document.createElement("div");
