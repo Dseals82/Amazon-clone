9535e79f150a640c37c6f634bfe5e52addb6ecdb
Tech Manager <techmanager@techs-mbp.myfiosgateway.com>
2021-08-04 11:42:41 -0400

    adding add to cart functionality

--- script.js
+++ script.js
@@ -16,14 +16,38 @@ function getItems(){
     });
 };
 
+function addToCart(item){
+    let cartItem = db.collection("cart-items").doc(item.id);
+    cartItem.get()
+    .then((doc) => {
+            if (doc.exists) {
+                cartItem.update({
+                    quantity: doc.data().quantity + 1
+                })
+            } else {
+                cartItem.set({
+                    image: item.image,
+                    make: item.make,
+                    name: item.name,
+                    rating: item.rating,
+                    price: item.price,
+                    quantity: 1
+                });
+            }
+        })
+
+    
+}
+
 function generateItems(items){
     let itemsHTML = "";
     items.forEach((item)=>{
-        itemsHTML += `
-        <div class="main-product">
+        let doc = document.createElement("div");
+        doc.classList.add("main-product");
+        doc.innerHTML = `
         <div class="product-image w-48 h-52 bg-white rounded-lg p-4">
             
-            <img class=" w-full h-full object-contain" src="${item.image}" alt="${item.name}">
+        <img class=" w-full h-full object-contain" src="${item.image}" alt="${item.name}">
         </div>
         <div class="product-name text-gray-700 font-bold mt-2 text-sm">
             ${item.name}
@@ -37,13 +61,20 @@ function generateItems(items){
         <div class="product-price font-bold text-gray-700 text-lg">
             $${item.price}
         </div>
-        <div class="add-to-cart h-8 w-28 bg-yellow-500 flex items-center justify-center text-white rounded text-md cursor-pointer hover:bg-yellow-600">
-            Add to cart
-        </div>
-    </div>
         `
+       let addToCartEl = document.createElement("div");
+       addToCartEl.classList.add("hover:bg-yellow-600",
+       "cursor-pointer","text-md","rounded","text-white",
+       "justify-center","items-center","flex","bg-yellow-500",
+       "add-to-cart","h-8","w-28");
+        addToCartEl.innerText = "Add to cart";
+        doc.appendChild(addToCartEl);
+        addToCartEl.addEventListener("click",function(){
+            addToCart(item);
+        });
+        document.querySelector(".main-section-products").appendChild(doc);
+        
     });
-    document.querySelector(".main-section-products").innerHTML = itemsHTML;
 };
 
 getItems();
